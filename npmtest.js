8f9be041baa5a5f69c548d6a81e52f6adb1ae233
Justin Allen <justinallen.ch@gmail.com>
2020-06-07 21:59:11 +0200

    first commit

---
+++ npmtest.js
@@ -0,0 +1,11 @@
+var http=require('http');
+
+var server=http.createServer(function(req,res){
+    res.end('test');
+});
+
+server.on('listening',function(){
+    console.log('ok, server is running');
+});
+
+server.listen(80);
