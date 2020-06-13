b2357b02dd292cd4f255a0f2a206b99c411ea0bb
Justin Allen <justinallen.ch@gmail.com>
2020-06-13 17:24:38 +0200

    Added features - deploy-infra

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STACK_NAME=awsbootstrap7
+STACK_NAME=awsbootstrap
 REGION=us-east-1
 CLI_PROFILE=awsbootstrap
 
@@ -8,7 +8,30 @@ EC2_INSTANCE_TYPE=t2.micro
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
 CODEPIPLINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
+# Generate a personal access token with repo and admin:repo_hook
+#   permissions from https://github.com/settins/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
+#Display GitHub environment variables
+echo -e "\n\n=========== Display GitHub environment variables ==========="
+echo -e "\n Access Token = $GH_ACCESS_TOKEN"
+echo -e "\n Owner = $GH_OWNER"
+echo -e "\n Repo = $GH_REPO"
+echo -e "\n Branch = $GH_BRANCH"
+
 #Deploy the Cloudformation template
+echo -e "\n\n=========== Deploying setup.yaml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides CodePipelineBucket=$CODEPIPLINE_BUCKET
 
 echo -e "\n\n=========== Deploying main.yaml ==========="
 aws cloudformation deploy \
@@ -17,17 +40,14 @@ aws cloudformation deploy \
   --stack-name $STACK_NAME \
   --template-file main.yml \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubBranch=$GH_BRANCH \
+    GitHubRepo=$GH_REPO \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPLINE_BUCKET
 
-echo -e "\n\n=========== Deploying setup.yaml ==========="
-aws cloudformation deply \
-  --region $REGION \
-  --profile $CLI_PROFILE \
-  --stack-name $STACK_NAME \
-  --template-file setup.yml \
-  --no-fail-on-empty-changeset \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides CodePipelineBucket=$CODEPIPLINE_BUCKET
+
 
 
 # If the deployment is succeeded, show the DNS name of the created instance
